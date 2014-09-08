78e37d79b41dbe761f6741a4e0e09c9af9c03098
Hélio <insign@gmail.com>
2014-09-08 00:42:29 -0300

    First fully working commit

--- forge-bell.user.js
+++ forge-bell.user.js
@@ -6,8 +6,10 @@
 // @match      https://forge.laravel.com/*
 // @require      https://cdn.jsdelivr.net/audio5js/0.1.7/audio5.min.js
 // @copyright  Hélio <insign@gmail.com> (Mozilla License 2)
-// @grant none
+// @grant unsafeWindow
 // ==/UserScript==
+
+var uW = unsafeWindow;
 
 var
     fb_dbutton,
@@ -16,31 +18,47 @@ var
     $          = jQuery
     ;
 
-var fb_on = new Audio5js({
+
+
+uW.fb_on = new Audio5js({
   ready: function () {
-    this.load('http://k007.kiwi6.com/hotlink/qtxvjf5osr/Speech_On.wav');
-    this.play();
+    this.load('https://insign.github.io/forge-bell/sounds/on.wav');
   }
 });
 
+uW.fb_off = new Audio5js({
+  ready: function () {
+    this.load('https://insign.github.io/forge-bell/sounds/off.wav');
+  }
+});
+console.info(unsafeWindow.fb_on);
 $(document).ready(function ($) {
   console.info('Forge Bell - STARTED');
 
   fb_dtimer = setInterval(function () {
     fg_check()
   }, 200);
+  
+  uW.fb_on();
+  uW.fb_off();
 });
 
 function fg_check() {
   fb_dbutton = $('[ng-click="deploySite()"]');
 
   if (fb_dstatus != 'waiting' && fb_dbutton.is(':contains("Deploy Now")')) {
+    if (fb_dstatus == 'deploying') {
+      uW.fb_off.play();
+    }
+    
     fb_dstatus = 'waiting';
     console.info('Forge Bell - Just waiting');
   }
   else if (fb_dstatus != 'queued' && fb_dbutton.is(':contains("Queued")')) {
     fb_dstatus = 'queued';
     console.info('Forge Bell - Preparing for deployment');
+    
+    uW.fb_on.play();
   }
   else if (fb_dstatus != 'deploying' && fb_dbutton.is(':contains("Deploying")')) {
     fb_dstatus = 'deploying';
